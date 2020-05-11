05da58f6d87c5ab4441894df6d9d0e6fbc9d9389
liman-collab <liman.kastrati98@gmail.com>
2020-05-11 04:57:06 +0200

    add item process,cart.action,cart.reducer,cart.types,collection-item,collection-preview

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -3,3 +3,8 @@ import CartActionsTypes from "./cart.types";
 export const toogleCartHidden = () => ({
   type: CartActionsTypes.TOOGLE_CART_HIDDEN,
 });
+
+export const addItem = (item) => ({
+  type: CartActionsTypes.ADD_ITEM,
+  payload: item,
+});
--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -2,6 +2,7 @@ import CartActionsTypes from "./cart.types";
 
 const INITIAL_STATE = {
   hidden: true,
+  cartItems: [],
 };
 
 const cartReducer = (state = INITIAL_STATE, action) => {
@@ -11,6 +12,13 @@ const cartReducer = (state = INITIAL_STATE, action) => {
         ...state,
         hidden: !state.hidden,
       };
+
+    case CartActionsTypes.ADD_ITEM:
+      return {
+        ...state,
+        cartItems: [...state.cartItems, action.payload],
+      };
+
     default:
       return state;
   }
