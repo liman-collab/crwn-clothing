4d8a601283cc8895deb752e28ad10c2e2de021a8
liman-collab <liman.kastrati98@gmail.com>
2020-06-12 01:43:40 +0200

    firebase,firestore,shop

--- src/components/custom-button/custom-button.component.js
+++ src/components/custom-button/custom-button.component.js
@@ -1,3 +1,12 @@
+// import React from "react";
+
+// import { CustomButtonContainer } from "./custom-button.styles";
+
+// const CustomButton = ({ children, ...props }) => (
+//   <CustomButtonContainer {...props}>{children}</CustomButtonContainer>
+// );
+
+// export default CustomButton;
 import React from "react";
 
 import "./custom-button.styles.scss";
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,12 +12,14 @@ const config = {
   appId: "1:840002479345:web:6bec32d3dbf437732df550",
   measurementId: "G-T2K3EZ33QV",
 };
+firebase.initializeApp(config);
 
 //api request => async action
 export const createUserProfileDocument = async (userAuth, additionalData) => {
   if (!userAuth) return;
 
   const userRef = firestore.doc(`users/${userAuth.uid}`);
+
   const snapShot = await userRef.get();
 
   //checking if there are data
@@ -39,7 +41,38 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
   return userRef;
 };
 
-firebase.initializeApp(config);
+export const addCollectionAndDocuments = async (
+  collectionKey,
+  objectsToAdd
+) => {
+  const collectionRef = firestore.collection(collectionKey);
+
+  const batch = firestore.batch();
+  objectsToAdd.forEach((obj) => {
+    const newDocRef = collectionRef.doc();
+    batch.set(newDocRef, obj);
+  });
+
+  return await batch.commit();
+};
+
+export const convertCollectionsSnapshotToMap = (collections) => {
+  const transformedCollection = collections.docs.map((doc) => {
+    const { title, items } = doc.data();
+
+    return {
+      routeName: encodeURI(title.toLowerCase()),
+      id: doc.id,
+      title,
+      items,
+    };
+  });
+
+  return transformedCollection.reduce((accumulator, collection) => {
+    accumulator[collection.title.toLowerCase()] = collection;
+    return accumulator;
+  }, {});
+};
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
